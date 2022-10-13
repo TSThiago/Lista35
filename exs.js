d0dc946c4e9b83eea8bcbe79e57a6775300330ed
Thiago Shibanuma <thiago.tshibanuma@gmail.com>
2022-10-13 20:16:48 -0300

    Término do exercício 1.

--- exs.js
+++ exs.js
@@ -1,28 +1,17 @@
 // Ex 1:
-// console.log(QuebrarChocolate(3,2))
-// function QuebrarChocolate(n: number, m: number): number {
-//     let contador: number = 0
-//     if (n == 0 || m == 0) {
-//         return -1
-//     } else {
-//         while (n >= 1 && m >= 1) {
-//             n = n - 1
-//             m = m - 1
-//             contador++
-//         }
-//         return contador
+console.log(QuebrarChocolate(4, 2));
+function QuebrarChocolate(n, m) {
+    return ((n * m) - 1);
+}
+// Ex 2:
+// console.log(TrocarLetras("CODigo"))
+// function TrocarLetras(palavra : string) : string{
+//     console.log(palavra)
+//     let arrayMinusculo : string[] = palavra.match(/[a-z]/g)
+//     let arrayMaiusculo : string[] = palavra.match(/[A-Z]/g)
+//     if(arrayMaiusculo.length > arrayMinusculo.length){
+//         return palavra.toUpperCase()
+//     }else{
+//         return palavra.toLowerCase()
 //     }
 // }
-// Ex 2:
-console.log(TrocarLetras("CODigo"));
-function TrocarLetras(palavra) {
-    console.log(palavra);
-    var arrayMinusculo = palavra.match(/[a-z]/g);
-    var arrayMaiusculo = palavra.match(/[A-Z]/g);
-    if (arrayMaiusculo.length > arrayMinusculo.length) {
-        return palavra.toUpperCase();
-    }
-    else {
-        return palavra.toLowerCase();
-    }
-}
--- exs.ts
+++ exs.ts
@@ -1,32 +1,23 @@
 // Ex 1:
 
-// console.log(QuebrarChocolate(3,2))
+console.log(QuebrarChocolate(4,2))
 
-// function QuebrarChocolate(n: number, m: number): number {
-//     let contador: number = 0
-//     if (n == 0 || m == 0) {
-//         return -1
-//     } else {
-//         while (n >= 1 && m >= 1) {
-//             n = n - 1
-//             m = m - 1
-//             contador++
-//         }
-//         return contador
-//     }
-// }
+function QuebrarChocolate(n: number, m: number): number {
+    return ((n * m) - 1)
+}
 
 // Ex 2:
 
-console.log(TrocarLetras("CODigo"))
+// console.log(TrocarLetras("CODigo"))
 
-function TrocarLetras(palavra : string) : string{
-    console.log(palavra)
-    let arrayMinusculo : string[] = palavra.match(/[a-z]/g)
-    let arrayMaiusculo : string[] = palavra.match(/[A-Z]/g)
-    if(arrayMaiusculo.length > arrayMinusculo.length){
-        return palavra.toUpperCase()
-    }else{
-        return palavra.toLowerCase()
-    }
-}
+// function TrocarLetras(palavra : string) : string{
+//     console.log(palavra)
+//     let arrayMinusculo : string[] = palavra.match(/[a-z]/g)
+//     let arrayMaiusculo : string[] = palavra.match(/[A-Z]/g)
+//     if(arrayMaiusculo.length > arrayMinusculo.length){
+//         return palavra.toUpperCase()
+//     }else{
+//         return palavra.toLowerCase()
+//     }
+// }
+
